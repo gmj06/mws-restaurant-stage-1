18e4f2d063ffd1130ed15ce3f75091adbbba1c1b
Gowri Jagadeesh <gowrimj2006@gmail.com>
2018-07-27 18:18:15 -0700

    moved files from app folder and deleted app folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var browerSync = require('browser-sync').create();
 
-gulp.task('default', function(){
-    gulp.watch('app/sass/**/*.scss', ['styles'])
+gulp.task('default', ['styles'], function(){
+    gulp.watch('sass/**/*.scss', ['styles']);
+
+    browerSync.init({
+        server: './'
+    });
 });
 
-gulp.task('styles', ['styles'], function(){
-    gulp.src('app/sass/**/*.scss')
+gulp.task('styles',  function(){
+    gulp.src('sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
         browsers: ['last 2 versions']
     }))
-    .pipe(gulp.dest('./app/css'))
+    .pipe(gulp.dest('./css'))
+    .pipe(browerSync.stream())
 });

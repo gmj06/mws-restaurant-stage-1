51acfcd074c50f53557cafb0b7fed13331e3d101
Gowri Jagadeesh <gowrimj2006@gmail.com>
2018-07-29 15:35:45 -0700

    installed gulp-concat, gulp-uglify, gulp-babel, gulp-eslint, gulp-imagemin, gulp-sourcemaps, imagemin-pngquant

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,96 @@
+/*eslint-env node */
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var browerSync = require('browser-sync').create();
+var browserSync = require('browser-sync').create();
+var eslint = require('gulp-eslint');
+var jasmine = require('gulp-jasmine-phantom');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
-gulp.task('default', ['styles'], function(){
-    gulp.watch('sass/**/*.scss', ['styles']);
+gulp.task('default', ['copy-html', 'copy-images', 'styles', 'lint', 'scripts'], function(){
+    gulp.watch('app/sass/**/*.scss', ['styles']);
+    gulp.watch('app/js/**/*.js', ['lint']);
+    gulp.watch('app/index.html', ['copy-html']);
+    gulp.watch('./dist/index.html').on('change', browserSync.reload);
 
-    browerSync.init({
-        server: './'
-    });
+    browserSync.init({
+        server: './dist'//,
+        //index: 'index.html'
+    });    
 });
 
-gulp.task('styles',  function(){
-    gulp.src('sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions']
+gulp.task('dist', [
+    'copy-html',
+    'copy-images',
+    'styles',
+    'lint',
+    'scripts-dist'
+]);
+
+gulp.task('copy-html', function(){
+    gulp.src('app/index.html')    
+    .pipe(gulp.dest('./dist'));
+    //.pipe(browserSync.stream());
+});
+
+gulp.task('copy-images', function(){
+    gulp.src('app/img/*')
+    .pipe(imagemin({
+        progressive: true,
+        use: [pngquant()]
     }))
-    .pipe(gulp.dest('./css'))
-    .pipe(browerSync.stream())
+    .pipe(gulp.dest('dist/img'));
 });
+
+gulp.task('styles', function(){
+    gulp.src('app/sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function(){
+    gulp.src('app/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/js'));
+});
+
+
+gulp.task('scripts-dist', function(){
+    gulp.src('app/js/**/*.js')
+    .pipe(babel())
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+gulp.task('lint', () => {
+    // ESLint ignores files with "node_modules" paths.
+    // So, it's best to have gulp ignore the directory as well.
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+    return gulp.src(['app/js/**/*.js','!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
